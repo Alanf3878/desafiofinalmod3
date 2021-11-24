7ff0843b109ed34a1c6d401b9d0269c8589f57cb
Alan Fernandes <81441721+Alanf3878@users.noreply.github.com>
2021-11-24 16:07:08 -0300

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,14 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "desafiofinal3",
   },
-  plugins: ["gatsby-plugin-styled-components"],
+  plugins: ["gatsby-plugin-styled-components",
+  {
+    resolve: "gatsby-source-graphql",
+    options: {
+      typeName: "SWAPI",    
+      fieldName: "swapi",     
+      url: "https://swapi-graphql.netlify.app/.netlify/functions/index",
+    }
+  }
+],
 };
